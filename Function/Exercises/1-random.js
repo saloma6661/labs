60ed1b9240480c288101388aceb68fc608144849
Mykhailo <solomkamisha516@gmail.com>
2024-12-13 00:26:04 +0200

    Delete Function/Exercises directory

--- Function/Exercises/1-random.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const random = (min, max) => {
-  // Generate random Number between from min to max
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
-};
-
-module.exports = { random };
